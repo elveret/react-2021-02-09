aac9e1e8c172291fd7cc04f128afce9f1d4b5d1e
elveret <lena.veret@gmail.com>
2021-02-23 21:01:01 +0300

    add some changes

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styles from '../menu/menu.module.css';
-import Product from '../product';
 
 const Basket = ({ sumOrder }) => {
   return <div></div>;
